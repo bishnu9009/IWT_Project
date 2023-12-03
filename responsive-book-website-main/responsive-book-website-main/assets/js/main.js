7398a483d392907c88bcf2d3ed2a380e2e4fc439
Bishnutosh <70653087+bishnu9009@users.noreply.github.com>
2023-12-03 13:04:38 +0530

    Initial Commit

---
+++ responsive-book-website-main/responsive-book-website-main/assets/js/main.js
@@ -0,0 +1,106 @@
+/*=============== SEARCH ===============*/
+const searchButton = document.getElementById('search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content')
+
+/* Menu show */
+if(searchButton){
+    searchButton.addEventListener('click', () =>{
+        searchContent.classList.add('show-search')
+    })
+}
+
+/* Menu hidden */
+if(searchClose){
+    searchClose.addEventListener('click', () =>{
+        searchContent.classList.remove('show-search')
+    })
+}
+
+/*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
+
+/* Login show */
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+
+/* Login hidden */
+if(loginClose){
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
+}
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView:'auto',
+    centeredSlides: 'auto',
+    
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction : false,
+    },
+
+
+    breakpoints:{
+        1220:{
+            spaceBetween: -32,
+        }
+    }
+})
+
+/*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper('.featured__swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView:'auto',
+    centeredSlides: 'auto',
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        1150:{
+            slidesPerView:4,
+            centeredSlides:false,
+        }
+    }
+})
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
