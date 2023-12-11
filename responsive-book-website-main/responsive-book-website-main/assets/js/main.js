f9ad462ceed6c4cc8366c3cb8c9dbbd4dcab9ffa
Bishnutosh <70653087+bishnu9009@users.noreply.github.com>
2023-12-11 09:32:27 +0530

    inspirations

--- responsive-book-website-main/responsive-book-website-main/assets/js/main.js
+++ responsive-book-website-main/responsive-book-website-main/assets/js/main.js
@@ -100,7 +100,29 @@ let swiperNew = new Swiper('.new__swiper', {
     }
 })
 
-/*=============== TESTIMONIAL SWIPER ===============*/
+/*=============== INSPIRATION ===============*/
+let swiperInspiration = new Swiper('.featured__swiper', {
+    loop: false,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView:'auto',
+    centeredSlides: 'auto',
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        1150:{
+            slidesPerView:4,
+            centeredSlides:false,
+        }
+    }
+})
+
+
+
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
